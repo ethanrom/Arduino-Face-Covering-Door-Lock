b824ae17070ce6c0a6b6f5818f421bc8d8142d06
ethanrom <116474403+ethanrom@users.noreply.github.com>
2023-02-19 19:23:22 +0530

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -1,79 +1,72 @@
 #include <Servo.h>
-#include <opencv2/opencv.hpp>
+#include <Wire.h>
+#include <OV7670.h>
+#include <Adafruit_MLX90614.h>
+#include <opencv2/objdetect.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 
-// Define the pins for the servo motor
-#define SERVO_PIN 9
+Servo myServo;  // Create a servo object
+OV7670 mycam;  // Create a camera object
+Adafruit_MLX90614 mlx = Adafruit_MLX90614(); // Create a temperature sensor object
+int servoPin = 9;  // Define the pin for the servo signal
+int angle = 0;  // Initialize the angle variable to 0
+int camPinSCL = A5;  // Define the pin for the camera SCL
+int camPinSDA = A4;  // Define the pin for the camera SDA
 
-// Create a Servo object
-Servo doorLock;
-
-// Define the OpenCV cascade classifiers for face and nose detection
-cv::CascadeClassifier faceCascade, noseCascade;
-
-// Define the OpenCV video capture object
-cv::VideoCapture camera(0);
-
-// Set the minimum face size for detection (in pixels)
-const int minFaceSize = 80;
+// Initialize the Haar cascade classifiers for face and nose detection
+cv::CascadeClassifier face_cascade;
+cv::CascadeClassifier nose_cascade;
 
 void setup() {
-  // Attach the servo to the pin
-  doorLock.attach(SERVO_PIN);
-
-  // Load the OpenCV cascade classifiers
-  faceCascade.load("haarcascade_frontalface_default.xml");
-  noseCascade.load("haarcascade_mcs_nose.xml");
-
-  // Open the camera
-  if (!camera.isOpened()) {
-    Serial.println("Could not open camera");
-    while (1);
-  }
+  myServo.attach(servoPin);  // Attach the servo to the designated pin
+  mycam.begin(camPinSCL, camPinSDA);  // Begin the camera object
+  mlx.begin(); // Begin the temperature sensor object
+  Serial.begin(9600);  // Begin serial communication
+  
+  // Load the face and nose detection Haar cascade classifiers
+  face_cascade.load("haarcascade_frontalface_default.xml");
+  nose_cascade.load("haarcascade_mcs_nose.xml");
 }
 
 void loop() {
-  // Capture a frame from the camera
-  cv::Mat frame;
-  camera >> frame;
+  uint16_t pixels[120 * 160];  // Create an array to store the camera pixels
+  int covered_nose = 0;  // Initialize the covered_nose variable to 0
+  mycam.takeRGBPicture(pixels);  // Take a picture with the camera
 
-  // Convert the frame to grayscale
+  // Convert the camera image to grayscale and detect faces and noses using the Haar cascade classifiers
+  cv::Mat img(120, 160, CV_8UC2, pixels);
   cv::Mat gray;
-  cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
+  cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
+  std::vector<cv::Rect> faces, noses;
+  face_cascade.detectMultiScale(gray, faces, 1.1, 2, 0 | cv::CASCADE_SCALE_IMAGE, cv::Size(30, 30));
+  nose_cascade.detectMultiScale(gray, noses, 1.1, 2, 0 | cv::CASCADE_SCALE_IMAGE, cv::Size(30, 30));
 
-  // Detect faces in the grayscale image
-  std::vector<cv::Rect> faces;
-  faceCascade.detectMultiScale(gray, faces, 1.3, 5);
-
-  // Check if any faces were detected
-  if (!faces.empty()) {
-    // Get the largest face in the image
-    cv::Rect largestFace;
-    int maxArea = 0;
-    for (const auto& face : faces) {
-      if (face.area() > maxArea) {
-        largestFace = face;
-        maxArea = face.area();
-      }
-    }
-
-    // Check if the face is large enough
-    if (largestFace.width > minFaceSize && largestFace.height > minFaceSize) {
-      // Detect the nose within the face region
-      cv::Mat faceROI = gray(largestFace);
-      std::vector<cv::Rect> noses;
-      noseCascade.detectMultiScale(faceROI, noses, 1.3, 5);
-
-      // Check if the nose was detected
-      if (!noses.empty()) {
-        // Rotate the servo to unlock the door
-        doorLock.write(90);
-
-        // Wait for a second
-        delay(1000);
-
-        // Rotate the servo back to lock the door
-        doorLock.write(0);
+  // Check if a nose is detected and if it is covered
+  if (noses.size() > 0) {
+    for (size_t i = 0; i < noses.size(); i++) {
+      cv::Rect nose = noses[i];
+      cv::Point nose_center(nose.x + nose.width/2, nose.y + nose.height/2);
+      for (size_t j = 0; j < faces.size(); j++) {
+        cv::Rect face = faces[j];
+        if (nose_center.x >= face.x && nose_center.x <= face.x + face.width &&
+            nose_center.y >= face.y && nose_center.y <= face.y + face.height) {
+          covered_nose = 1;
+          break;
+        }
       }
     }
   }
+
+  if (covered_nose) {  // If a covered nose is detected
+    angle = 90;
+    myservo.write(angle);  // Lock the door by rotating the servo 90 degrees
+    delay(1000);  // Wait for 1 second
+  } else {  // If a covered nose is not detected
+    angle = 0;
+    myservo.write(angle);  // Unlock the door by rotating the servo back to 0 degrees
+    delay(1000);  // Wait for 1 second
+  }
 }
+
+   
